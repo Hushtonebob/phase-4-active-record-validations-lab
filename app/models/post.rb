955c61aac7ebb36a774eded99eced1b02a640054
Bobby De Luna <bobby.deluna2@gmail.com>
2023-02-08 15:50:09 -0700

    Completed Assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,16 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :category, inclusion: {in:["Fiction","Non-Fiction"]}
+    validates :content, length: {minimum:250}
+    validates :summary, length: {maximum:250}
+    validate :not_allowed
+
+
+    def not_allowed
+        if title == "True Facts"
+            errors.add(:title, "Please choose another title.")
+        end
+    end
+
 end
